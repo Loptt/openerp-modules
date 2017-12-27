f9a55ea5f917ef1d084618af172cee65d0189bb0
Loptt <carlos.deg02@hotmail.com>
2017-12-27 13:44:07 -0800

    Added first test module

---
+++ Modules/primer_modulo/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    "name" : "Modulo de Prueba",
+    "version" : "1.0",
+    "author" : "Carlos",
+    "category" : "Test",
+    "website" : "None",
+    "description": "Primer modulo de prueba.",
+    "depends" : ["base"],
+    "init_xml" : [],
+    "update_xml" : ["primer_modulo_view.xml"],
+    "active": True,
+    "installable": True
+}
---
+++ Modules/primer_modulo/product_add.py
@@ -0,0 +1,12 @@
+from osv import fields, osv
+
+class product_add(osv.osv):
+    _inherit = 'product.product'
+
+    _columns = {'prueba' : fields.char('Prueba', size=128, translate=True, help='Hola'),
+    }
+
+    #_defaults = { 'prueba' : 'Estaba vacio, ya no.',
+   #}
+    
+product_add()
