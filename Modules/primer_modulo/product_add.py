0211b60c9ed159a54385f828953a481948fd73ea
Loptt <carlos.deg02@hotmail.com>
2017-12-28 13:53:50 -0600

    Fixed syntaxis on XML file and added default values in product_add.py

--- Modules/primer_modulo/product_add.py
+++ Modules/primer_modulo/product_add.py
@@ -6,7 +6,7 @@ class product_add(osv.osv):
     _columns = {'prueba' : fields.char('Prueba', size=128, translate=True, help='Hola'),
     }
 
-    #_defaults = { 'prueba' : 'Estaba vacio, ya no.',
-   #}
+    _defaults = { 'prueba' : 'Estaba vacio, ya no.',
+    }
     
 product_add()
